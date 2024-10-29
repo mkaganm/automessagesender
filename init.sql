6f1a6337ce4cbde1dfbe6287bb0fdb06408924fa
mkaganm <mustafakaganmeric@gmail.com>
2024-10-29 22:28:34 +0300

    update docker compose and add init.sql

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS automessagesender;
+USE automessagesender;
+
+CREATE TABLE IF NOT EXISTS messages (
+                                        id INT AUTO_INCREMENT PRIMARY KEY,
+                                        message_id VARCHAR(255) NOT NULL,
+    number VARCHAR(255) NOT NULL,
+    context LONGTEXT,
+    is_send BOOLEAN DEFAULT 0,
+    is_processed BOOLEAN DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
