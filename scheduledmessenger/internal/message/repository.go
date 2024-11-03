446f6dfbaeb9bfbba678b0a1e61612b19fd18b08
mkaganm <mustafakaganmeric@gmail.com>
2024-11-03 21:53:23 +0300

    update repository

--- scheduledmessenger/internal/message/repository.go
+++ scheduledmessenger/internal/message/repository.go
@@ -1,47 +1,28 @@
 package message
 
 import (
-	"go.uber.org/dig"
-	"log"
+	"scheduledmessenger/internal/container"
 	"scheduledmessenger/internal/db"
 )
 
-// Repository represents the message repository with a shared MySQLClient instance
-type Repository struct {
+// MessageRepository represents the message repository with a shared MySQLClient instance
+type MessageRepository struct {
 	client *db.MySQLClient
 }
 
-type RepositoryParams struct {
-	dig.In
-	DBClient *db.MySQLClient
-}
-
-var (
-	repoInstance *Repository
-)
-
-// CreateRepository sets up the singleton instance of Repository
-func CreateRepository(p RepositoryParams) *Repository {
-	return &Repository{client: p.DBClient}
-}
-
-// GetRepository returns the singleton instance of Repository
-func GetRepository() *Repository {
-	if repoInstance == nil {
-		log.Fatal("Repository not initialized. Call CreateRepository() first.")
-	}
-	return repoInstance
+func CreateRepository(c *container.Container) *MessageRepository {
+	return &MessageRepository{client: c.MySQLClient}
 }
 
 // GetUnsentMessages retrieves messages that have not been sent
-func (r *Repository) GetUnsentMessages() ([]Message, error) {
+func (r *MessageRepository) GetUnsentMessages() ([]Message, error) {
 	var messages []Message
 	err := r.client.Find(&messages, "is_send = ?", false)
 	return messages, err
 }
 
 // GetUnsentTwoMessages retrieves the first 2 unsent messages, ordered by created_at
-func (r *Repository) GetUnsentTwoMessages() ([]Message, error) {
+func (r *MessageRepository) GetUnsentTwoMessages() ([]Message, error) {
 	var messages []Message
 	err := r.client.Query().
 		Where("is_send = ?", false).
@@ -52,7 +33,13 @@ func (r *Repository) GetUnsentTwoMessages() ([]Message, error) {
 }
 
 // MarkAsSent updates a message to mark it as sent
-func (r *Repository) MarkAsSent(msg *Message) error {
+func (r *MessageRepository) MarkAsSent(msg *Message) error {
+	msg.IsSend = true
+	return r.client.Update(msg)
+}
+
+// MarkAsProcessed updates a message to mark it as processed
+func (r *MessageRepository) MarkAsProcessed(msg *Message) error {
 	msg.IsSend = true
 	return r.client.Update(msg)
 }
